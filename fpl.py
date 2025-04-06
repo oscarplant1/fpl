80dd653e9ea484957e19d9292c8f2122bac78022
oscarplant1 <oscarplant902@gmail.com>
2025-04-06 02:44:04 +0100

    06/04/25

--- fpl.py
+++ fpl.py
@@ -5,7 +5,7 @@ from GW_Squad import GW_Squad
 from Plan import Plan
 import time
 import jupyter
-
+import dearpygui.dearpygui as dpg
 #newPlan = Plan(566579)
 
 #Salah = Player(328)
@@ -19,3 +19,27 @@ import jupyter
 #testList.display_List()
 
 # %%
+dpg.create_context()
+dpg.create_viewport(title='Custom Title')
+
+with dpg.window(label="MainWindow",width = 375, height=200):
+    dpg.add_text("Enter Team ID",tag="Team_ID_Instruction")
+
+    input_field = dpg.add_input_text(label="Enter ID",tag="input_box")
+    
+    def on_button_click(sender, app_data):
+        entered_text = dpg.get_value(input_field)
+        if entered_text.isdigit():
+            dpg.delete_item("Submit_button")
+            dpg.delete_item("input_box")
+            dpg.delete_item("Team_ID_Instruction")
+            currentPlan = Plan(int(entered_text))
+
+
+    dpg.add_button(label="Submit",callback=on_button_click, tag="Submit_button")
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.maximize_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
